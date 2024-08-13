14c6981d3eed7517a5dd9d5b6686d942f74a065b
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-08-13 19:42:22 +0300

    Add project files.

---
+++ List1Practise/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// create new string list
+List<string> davetliler = new List<string>()
+{
+    "Bülent Ersoy",
+    "Ajda Pekkan",
+    "Ebru Gündeş",
+    "Hadise",
+    "Hande Yener",
+    "Tarkan",
+    "Funda Arar",
+    "Demet Akalın"
+};
+
+// we can also Add() methods
+
+davetliler.Add("Ceza");
+davetliler.Add("Tan Taşçı");
+
+// print all guest list
+
+Console.WriteLine("---DAVETLİLER---");
+foreach (var item in davetliler)
+{
+    Console.WriteLine(item);
+}
+
+
+
